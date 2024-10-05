bdf6238621c83e2c23ea6532bfa9f1dab41aefdd
Tomáš Reinert <reinerttomas@gmail.com>
2024-10-05 07:48:55 +0200

    clear

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,20 +4,9 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-use App\Models\Task;
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 final class DatabaseSeeder extends Seeder
 {
-    public function run(): void
-    {
-        User::factory()->count(3)->create();
-
-        Task::factory()
-            ->count(100)
-            ->recycle(User::all())
-            ->create();
-    }
+    public function run(): void {}
 }
